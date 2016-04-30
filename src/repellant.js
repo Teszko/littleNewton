deb369a3c4a218414ea3a14b6a9d577020c25843
Henryk Iwaniuk <b.iwaniuk@campus.tu-berlin.de>
2016-04-30 23:29:57 +0200

    added repellant force modifier

---
+++ src/repellant.js
@@ -0,0 +1,33 @@
+/**
+ * @author Henryk Iwaniuk / b.iwaniuk@campus.tu-berlin.de
+ */
+
+NEWTON.Repellant = function (b1) {
+    this.body = b1;
+    this.exclude = [this.body];
+};
+
+NEWTON.Repellant.prototype = {
+    constructor: NEWTON.Rubberband
+};
+
+NEWTON.Repellant.prototype.__applyForce = function (world) {
+    for (var i=0; i<world.__bodies.length; i++) {
+        var body2 = world.__bodies[i];
+        if (world.indexOf(body2) < 0) {
+            this.__applyForce_to_bodies(body2);
+        }
+    }
+};
+
+NEWTON.Repellant.prototype.__applyForce_to_bodies = function (body2) {
+     var dV = new NEWTON.v3d();
+     dV.subtract(body2.position, this.body.position);
+     dV.multiply(this.force(dV.length())/dV.length());
+     this.body.applyForceVector(dV.multiply(-1));
+     body2.applyForceVector(dV);
+};
+
+NEWTON.Repellant.prototype.force = function (d) {
+    return 4 * d * d;
+};
