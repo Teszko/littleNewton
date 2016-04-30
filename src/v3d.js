947614e39ac60ea5732889549ee03acb83982d32
Henryk Iwaniuk <b.iwaniuk@campus.tu-berlin.de>
2016-04-30 15:43:54 +0200

    applied changes to v3d add, shortened v3d multiply

--- src/v3d.js
+++ src/v3d.js
@@ -43,32 +43,37 @@ NEWTON.v3d.prototype.copy = function (A) {
 
 NEWTON.v3d.prototype.multiply = function (a) {
     if (NEWTON.isNumber(a)) {
-        NEWTON.v3d.__multiplyNum(a);
+        this.x *= a;
+        this.y *= a;
+        this.z *= a;
     }
 
     return this;
 };
 
-NEWTON.v3d.prototype.__multiplyNum = function (a) {
-    this.x *= a;
-    this.y *= a;
-    this.z *= a;
-    
-    return this;
-};
+/**
+ * add     this = A + B if B defined or this = this + A if not.
+ * @param A
+ * @param B     undefined or second vector
+ * @returns {NEWTON.v3d}
+ */
 
-NEWTON.v3d.prototype.add = function (A) {
+NEWTON.v3d.prototype.add = function (A, B) {
     if (NEWTON.isVector(A)) {
-        NEWTON.v3d.__addVector(A);
+        if (NEWTON.isVector(B)) {
+            this.__addVectors(A, B);
+        } else {
+            this.__addVectors(this, A);
+        }
     }
 
     return this;
 };
 
-NEWTON.v3d.prototype.__addVector = function (A) {
-    this.x += A.x;
-    this.y += A.y;
-    this.z += A.z;
+NEWTON.v3d.prototype.__addVectors = function (A, B) {
+    this.x = A.x + B.x;
+    this.y = A.y + B.y;
+    this.z = A.z + B.z;
 
     return this;
 };
